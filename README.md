bf57cecc0c865aec051efbd8c0ae6bae8e5113c1
k-malov <k-malov@ytsaurus.tech>
2023-08-07 19:25:22 +0300

    [yt/terraform-provider-ytsaurus] add installation instructions for terraform-mirror installation and fix typos and sad mistakes

--- README.md
+++ README.md
@@ -1,24 +1,52 @@
 # terraform-provider-ytsaurus
 
-## Prerequirements
+## Installation from Terraform mirror
+### Example layout
+- **\$HOME/.terraformrc** - Terraform configuration file.
+- **$HOME/tf** - a folder with Terraform files with your infrastructure configuration.
+
+### How to configure ~/.terraformrc
+```
+ $ cat ~/.terraformrc
+provider_installation {
+  network_mirror {
+    url = "https://terraform-provider.ytsaurus.tech"
+    include = ["registry.terraform.io/ytsaurus/*"]
+  }
+  direct {
+    exclude = ["registry.terraform.io/ytsaurus/*"]
+  }
+
+}
+```
+### How to download YTsaurus terraform-provider
+Run from your Terroform configuration
+```
+ $ cd $HOME/tf
+ $ terraform init
+
+```
+
+## Installation from source code
+### Prerequirements
 
 Below is a list of things you should have or configure:
 
 * golang
 * GOPATH environment variable
-* terraform binary
+* Terraform binary
 * YTsaurus main repo
-* YTsaurus cluster for acceptance tests 
+* YTsaurus cluster for acceptance tests
 
 ### Example layout
 
-- **\$HOME/.terraformrc** - terraform configuration file with dev_overrides.
+- **\$HOME/.terraformrc** - Terraform configuration file.
 - **\$HOME/go** - golang home, and GOPATH value.
-- **\$HOME/go/src/terraform-provider-ytsaurus** - a folder with provider's source code from github.com:ytsaurus/terraform-provider-ytsaurus.git.
+- **\$HOME/go/src/terraform-provider-ytsaurus** - a folder with the provider's source code from github.com:ytsaurus/terraform-provider-ytsaurus.git.
 - **$HOME/ytsaurus** - a folder with terraform-provider-ytsaurus checked out from github.com:ytsaurus/terraform-provider-ytsaurus.git.
-- **$HOME/tf** - a folder with terraform files with your infrastructure configuration.
+- **$HOME/tf** - a folder with Terraform files with your infrastructure configuration.
 
-## How to run tests
+### How to run tests
 
 ```
 ~/ytsaurus/yt/docker/local/run_local_cluster.sh
@@ -26,14 +54,14 @@ cd ~/go/src/terraform-provider-ytsaurus
 make test
 ```
 
-## How to install
+### How to install
 
 ```
 cd ~/go/src/terraform-provider-ytsaurus
-make install 
+make install
 ```
 
-## How to configure
+### How to configure ~/.terraformrc
 Add dev_overrides to ~/.terraformrc
 ```
  $ cat ~/.terraformrc
--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-# Add some size constants
+# Add some size constants.
 variable "MBi" {
   type    = number
   default = 1024 * 1024
@@ -18,26 +18,26 @@ variable "GBi" {
 }
 
 # There are two options for YTsaurus provider configuration:
-# - cluster - YTsaurus cluster's fqdn
-# - token - Admin's token
-#
-# For security reasons do not store 'token' option in .tf file.
-# Instead, the 'token' should be stored in $HOME/.yt/token file with the appropriate file permissions.
+# cluster - YTsaurus cluster's fqdn
+# token - Admin's token
+
+# For security reasons, do not store the token option in .tf file.
+# Instead, the 'token' should be stored in the $HOME/.yt/token file with the appropriate file permissions.
 
 provider "ytsaurus" {
   cluster = "<FQDN>"
 }
 
-# Suppose we wants to develop a web app for a pet store management called 'Mr.Cat'.
-# And out two developers, Alisa and Denis, are going to use YTsaurus for it.
-# Let's create YTsaurus objects for Mr.Cat project with terraform.
+# Suppose we want to develop a web app for pet store management called 'Mr.Cat'.
+# And our two developers, Alisa and Denis, are going to use YTsaurus for it.
+# Let's create YTsaurus objects for Mr.Cat's project with Terraform.
 
-# First we will create a group 'mrcat'
+# First, we will create a group named 'mrcat'.
 resource "ytsaurus_group" "mrcat_main_group" {
   name = "mrcat"
 }
 
-# And users for Alisa and Denis
+# And users for Alisa and Denis.
 resource "ytsaurus_user" "mrcat_alisa" {
   name = "alisa"
   member_of = [
@@ -52,7 +52,7 @@ resource "ytsaurus_user" "mrcat_denis" {
   ]
 }
 
-# Now we can create an account with storage resources allowed for out project and create a home dir.
+# Now we can create an account with the storage resources allowed for our project and create a home directory.
 # But first, we should create some ACLs.
 
 variable "acl_mrcat_use" {
@@ -112,11 +112,11 @@ resource "ytsaurus_map_node" "mrcat_home" {
   }
 }
 
-# Alisa wants a very fast Key-Value storage to improve web app's user experience.
+# Alisa wants very fast Key-Value storage to improve the web app's user experience.
 # And YTsaurus dynamic tables are suitable for this task.
 
-# But dynamic tables won't work well on HDD, already loaded with MapReduce batch processing tasks.
-# So a new media are needed. One medium for journals (WAL) and one for data.
+# But dynamic tables won't work well on HDDs, already loaded with MapReduce batch processing tasks.
+# So new media are needed. One medium for journals (WAL) and one for data.
 
 resource "ytsaurus_medium" "ssd_journals_medium" {
   name = "ssd_journals"
@@ -128,7 +128,7 @@ resource "ytsaurus_medium" "ssd_data_medium" {
   acl = var.acl_mrcat_use
 }
 
-# To dedecate some cluster's recources for special user a tablet cell bundle should be created.
+# To dedicate some of the cluster's resources to a special user, a tablet cell bundle should be created.
 resource "ytsaurus_tablet_cell_bundle" "mrcat_tcb" {
   name              = "mrcat"
   tablet_cell_count = 1
@@ -142,10 +142,10 @@ resource "ytsaurus_tablet_cell_bundle" "mrcat_tcb" {
   }
 }
 
-# While Alisa is working on the site and Key-Value storage, Denis wants to do analytics of user request logs.
+# While Alisa is working on the site and Key-Value storage, Denis wants to do analytics on user request logs.
 # He wants to use a MapReduce task to process logs stored in static tables in YTsaurus storage.
 # And he wants some guarantees for the running time of this task.
-# To give such guarantees we need to configure a pool for Mr.Cat's tasks with some amount of cluster's CPU and memory resources.
+# To give such guarantees, we need to configure a pool for Mr.Cat's tasks with some of the cluster's CPU and memory resources.
 
 resource "ytsaurus_scheduler_pool" "mrcat_main_pool" {
   name = "mrcat_main"
